2f76c3f6a28312a9821c1d505cba8959d76f97f9
Curso de Verão <cursodeverao@A102-19L.imd.local>
2019-01-20 09:46:54 -0300

    api v1

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('posts', views.PostModelViewSet)
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('api/v1', include(router.urls))
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from rest_framework import viewsets
+from .serializers import PostModelSerializer
+
 
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+class PostModelViewSet(viewsets.ModelViewSet):
+	serializer_class = PostModelSerializer
+	queryset = Post.objects.all().order_by('-published_date')
