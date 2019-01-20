359f6536c0e6879f71999f190ee2ed074bebbc21
Curso de Verão <cursodeverao@A102-19L.imd.local>
2019-01-20 10:41:35 -0300

    create post form & template

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,6 @@ router.register('posts', views.PostModelViewSet)
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
     path('api/v1', include(router.urls))
 ]
